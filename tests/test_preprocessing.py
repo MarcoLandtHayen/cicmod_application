a62058ac271d7ceb98a1d89e5021223896964e2b
MarcoLandtHayen <malaha@gmx.net>
2022-09-13 20:52:15 +0200

    Add function to split sequence

---
+++ tests/test_preprocessing.py
@@ -0,0 +1,56 @@
+import numpy as np
+import pytest
+
+from cicmod_application.preprocessing import split_sequence
+
+
+# Dummy example data
+#
+# data = [0.5, 1, -0.3, -0.7]
+# dim: only 'time'
+# name: 'SAM'
+
+
+@pytest.fixture
+def example_ndarray():
+
+    data = np.array(
+        [
+            [1,5],
+            [2,6],
+            [3,7],
+            [4,8],
+        ]
+    )
+    
+    return data
+
+
+@pytest.fixture
+def example_split_ndarray():
+
+    data = np.array(
+        [
+            [
+                [1, 5],
+                [2, 6],
+                [3, 7]
+            ],
+            [
+                [2, 6],
+                [3, 7],
+                [4, 8]
+            ]
+        ]
+    )
+    
+    return data
+
+
+def test_split_sequence(example_ndarray, example_split_ndarray):
+    """Ensure that function split_sequence splits given sequence correctly into samples of specified length."""
+
+    assert (
+        split_sequence(example_ndarray,3)
+        == example_split_ndarray
+    ).all()
